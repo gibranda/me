67978b13b7c84c972756ebeecbc741c23d86a11d
gibranda94 <gibranda94@gmail.com>
2020-02-08 17:02:06 +0700

    Updates

---
+++ precache-manifest.9093133dfd0c6d122dea32262ead018c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24af05de7b20e842f7b43d1791980ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab71b424d75762a80508",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "83dc6c149e047d97b57e",
+    "url": "/static/js/2.8ede1c79.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8ede1c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab71b424d75762a80508",
+    "url": "/static/js/main.28dafbc9.chunk.js"
+  },
+  {
+    "revision": "b2fb3e6d001ecef7364f",
+    "url": "/static/js/runtime-main.a1a920d6.js"
+  }
+]);
